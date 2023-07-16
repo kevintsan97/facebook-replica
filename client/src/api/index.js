0b8315d4ec50318324cbf754cd1245060aa3ad63
kevintsan97 <taitsan2406@gmail.com>
2023-07-16 15:51:40 -0700

    Replace localhost endpoint with server deployed endpoints

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API = axios.create({ baseURL: 'http://localhost:5000'})
+const API = axios.create({ baseURL: 'https://memories-app-tai-tsan-bc950337c6b6.herokuapp.com'})
 API.interceptors.request.use((req) =>{
     if(localStorage.getItem('profile')){
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
